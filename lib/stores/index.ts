00e2a93a6e65d351e1da50eeb6cff34d49568dbd
maikCyphlock <maikolaguilar656@gmail.com>
2025-08-16 12:57:49 -0400

    checkpoint

--- lib/stores/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-exportar tipos
-export type { AuthenticatedUser, AuthState, UserProfile } from '@/types/auth'
-export { useAuthStore } from './authStore'
-export { useQRStore } from './qrStore'
-export { useUIStore } from './uiStore'
